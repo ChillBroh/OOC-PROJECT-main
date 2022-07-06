b1e1f13d44f4f9cd16d3675df71e6199354abef5
Ishara Madusanka <93942615+IT21189944@users.noreply.github.com>
2022-07-07 00:27:21 +0530

    header file for manager is done

---
+++ OOC-PROJECT-main/Manager.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "User.h"
+#include "Report.h"
+class Manager : public User
+{
+private:
+    char Manager_ID[10];
+
+public:
+    Manager();
+    Manager(char M_username[], char M_pass[], char M_ID[]);
+    void display();
+    void ManageDonation();
+    void generateReport();
+    ~Manager();
+};
